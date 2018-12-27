d98b156fca752ce5e722646888c167c70a3cb874
Paul van Assen <source@pvanassen.nl>
2018-12-27 13:12:57 +0100

    Brightness controller

--- src/main/kotlin/nl/pvanassen/christmas/tree/controller/controller/BrightnessController.kt
+++ src/main/kotlin/nl/pvanassen/christmas/tree/controller/controller/BrightnessController.kt
@@ -1,8 +1,34 @@
 package nl.pvanassen.christmas.tree.controller.controller
 
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Post
+import nl.pvanassen.christmas.tree.controller.model.BrightnessState
+import nl.pvanassen.christmas.tree.controller.model.StripsModel
+import nl.pvanassen.christmas.tree.controller.scheduler.BrightnessService
 
 @Controller("brightness")
-class BrightnessController {
+class BrightnessController(private val stripsModel: StripsModel,
+                           private val brightnessService: BrightnessService) {
 
+    @Post("max")
+    fun maxBrightness(): HttpStatus {
+        BrightnessState.state = BrightnessState.State.MAX
+        stripsModel.setBrightness(1f)
+        return HttpStatus.OK
+    }
+
+    @Post("min")
+    fun minBrightness(): HttpStatus {
+        BrightnessState.state = BrightnessState.State.MIN
+        stripsModel.setBrightness(0.1f)
+        return HttpStatus.OK
+    }
+
+    @Post("auto")
+    fun autoBrightness(): HttpStatus {
+        BrightnessState.state = BrightnessState.State.AUTO
+        brightnessService.autoAdjustBrightness()
+        return HttpStatus.OK
+    }
 }
